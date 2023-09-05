542845b5b50b154c628540a9da2275797095b932
larcondo <larcondo90@gmail.com>
2023-09-05 14:41:56 -0300

    add Table component in Notes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {
   Navigate,
   useMatch
 } from 'react-router-dom'
+import { Table } from 'react-bootstrap'
 
 const Home = () => (
   <div>
@@ -27,13 +28,20 @@ const Note = ({ note }) => {
 const Notes = ({ notes }) => (
   <div>
     <h2>Notes</h2>
-    <ul>
+    <Table striped>
+      <tbody>
       { notes.map( note => 
-        <li key={note.id}>
-          <Link to={`/notes/${note.id}`}>{ note.content }</Link>
-        </li>  
+        <tr key={ note.id }>
+          <td>
+            <Link to={`/notes/${note.id}`}>{ note.content }</Link>
+          </td>
+          <td>
+            { note.user }
+          </td>
+        </tr>
       )}
-    </ul>
+      </tbody>
+    </Table>
   </div>
 )
 
