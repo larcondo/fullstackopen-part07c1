fddcddb7ed2c256306a2d3dabb83a3f77559c560
larcondo <larcondo90@gmail.com>
2023-09-05 15:12:31 -0300

    add Form and Button component

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import {
   Navigate,
   useMatch
 } from 'react-router-dom'
-import { Table } from 'react-bootstrap'
+import { Table, Button, Form } from 'react-bootstrap'
 
 const Home = () => (
   <div>
@@ -68,17 +68,17 @@ const Login = (props) => {
   return(
     <div>
       <h2>Login</h2>
-      <form onSubmit={onSubmit}>
-        <div>
-          username
-          <input />
-        </div>
-        <div>
-          password
-          <input type="password" />
-        </div>
-        <button type="submit">login</button>
-      </form>
+      <Form onSubmit={onSubmit}>
+        <Form.Group>
+          <Form.Label>username:</Form.Label>
+          <Form.Control type='text' name='username' />
+          <Form.Label>password:</Form.Label>
+          <Form.Control type='password' />
+          <Button variant='primary' type='submit'>
+            login
+          </Button>
+        </Form.Group>
+      </Form>
     </div>
   )
 }
